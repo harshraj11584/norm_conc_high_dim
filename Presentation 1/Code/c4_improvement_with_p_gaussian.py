ac2f9cf2c80e7d72d6a909c490d765478e7a6110
Harsh <harshraj11584@gmail.com>
2019-11-16 01:20:05 +0530

    showed Gaussian failing for high dim

--- Presentation 1/Code/c4_improvement_with_p_gaussian.py
+++ Presentation 1/Code/c4_improvement_with_p_gaussian.py
@@ -12,14 +12,15 @@ df1 = lambda x : get_distances(x,order=2.0)
 
 for dim in dimensions:
 	sigma_data=1.0
-	actual_data = np.random.multivariate_normal( mean=np.zeros(dim), cov=sigma_data**2 * np.eye(dim), size = N ) 
+	actual_data = np.random.multivariate_normal(mean=np.zeros(dim), cov=sigma_data**2 * np.eye(dim), size=N) 
 	actual_distances = df1(actual_data) 
 
-	#estimating hyperparameter sigma using thumb rule in paper 
+	#estimating hyperparameters p, sigma using thumb rule in paper 
 	d_5, d_50, d_95 = np.percentile(actual_distances,5),np.percentile(actual_distances,50),np.percentile(actual_distances,95)
-	sigma = d_50/((-np.log(0.5))**0.5)
-	sigma1 = d_5/((-np.log(0.05))**0.5)
-	sigma2 = d_95/((-np.log(0.95))**0.5)
+	p = np.log(np.log(0.05)/np.log(0.95)) / np.log(d_95/d_5) 
+	sigma = d_50/((-np.log(0.50))**(1/p))
+	sigma1 = d_5/((-np.log(0.05))**(1/p))
+	sigma2 = d_95/((-np.log(0.95))**(1/p))
 
 
 	dist_vals = np.linspace( 0, np.max( (np.max(actual_distances)+5, 15) ) , 100)
@@ -35,7 +36,7 @@ for dim in dimensions:
 	ax.hist(actual_distances,bins=200,density=True,label="Distribution of Distances") 
 	ax.axvline(d_5,ls='--')
 	ax.axvline(d_95,ls='--')
-	ax.set_title("Gaussian (Dimension="+str(dim)+")")
+	ax.set_title("Dimension="+str(dim))
 	ax.legend() 
 	fig.savefig('../Graphs/g3_problem_with_Gaussian_dim='+str(dim)+'.png')
 
