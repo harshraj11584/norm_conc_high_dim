0c41d530f44dd363df0b5edfdabf73461424635d
Harsh <harshraj11584@gmail.com>
2019-11-16 02:57:18 +0530

    classical kernels now destroyed

---
+++ Presentation 1/Code/c5_problems_in_general_kernels.py
@@ -0,0 +1,135 @@
+import numpy as np
+import numpy as np
+import matplotlib.pyplot as plt
+from math import pi,e
+from kernel_implementations import * 
+
+dimensions = [ 2, 10, 100 ]
+N = 10000
+
+df1 = lambda x : get_distances(x,order=2.0) 
+
+
+for dim in dimensions:
+	sigma_data=1.0
+	actual_data = np.random.multivariate_normal(mean=np.zeros(dim), cov=sigma_data**2 * np.eye(dim), size=N) 
+	actual_distances = df1(actual_data) 
+	d_5, d_50, d_95 = np.percentile(actual_distances,5),np.percentile(actual_distances,50),np.percentile(actual_distances,95)
+
+	dist_vals = np.linspace( 0, np.max(actual_distances)+5 , 100)
+	kernel_vals = polynomial_kernel(dist_vals) 
+	kernel_vals = kernel_vals/np.max(kernel_vals)
+
+	fig,ax=plt.subplots()
+	ax.plot(dist_vals,kernel_vals,label="Kernel Values") 
+	ax.hist(actual_distances,bins=200,density=True,label="Distribution of Distances") 
+	ax.axvline(d_5,ls='--')
+	ax.axvline(d_95,ls='--')
+	ax.set_title("Polynomial (Dim="+str(dim)+")") 
+	ax.legend() 
+	fig.savefig('../Graphs/g5_problem_polynomial_dim='+str(dim)+'.png')
+
+
+for dim in dimensions:
+	sigma_data=1.0
+	actual_data = np.random.multivariate_normal(mean=np.zeros(dim), cov=sigma_data**2 * np.eye(dim), size=N) 
+	actual_distances = df1(actual_data) 
+	d_5, d_50, d_95 = np.percentile(actual_distances,5),np.percentile(actual_distances,50),np.percentile(actual_distances,95)
+
+	dist_vals = np.linspace( 0, np.max(actual_distances)+5 , 100)
+	kernel_vals = rational_quadratic_kernel(dist_vals,c=(d_50)**2) 
+	kernel_vals = kernel_vals/np.max(kernel_vals)
+
+	fig,ax=plt.subplots()
+	ax.plot(dist_vals,kernel_vals,label="Kernel Values") 
+	ax.hist(actual_distances,bins=200,density=True,label="Distribution of Distances") 
+	ax.axvline(d_5,ls='--')
+	ax.axvline(d_95,ls='--')
+	ax.set_title("Rational Quadratic (Dim="+str(dim)+")")
+	ax.legend() 
+	fig.savefig('../Graphs/g5_problem_RationalQuadratic_dim='+str(dim)+'.png')
+
+
+for dim in dimensions:
+	sigma_data=1.0
+	actual_data = np.random.multivariate_normal(mean=np.zeros(dim), cov=sigma_data**2 * np.eye(dim), size=N) 
+	actual_distances = df1(actual_data) 
+	d_5, d_50, d_95 = np.percentile(actual_distances,5),np.percentile(actual_distances,50),np.percentile(actual_distances,95)
+
+	dist_vals = np.linspace( 0, np.max(actual_distances)+5 , 100)
+	kernel_vals = inverse_multiquadratic_kernel(dist_vals,c=(d_50)/(3**0.5)) 
+	kernel_vals = kernel_vals/np.max(kernel_vals)
+
+	fig,ax=plt.subplots()
+	ax.plot(dist_vals,kernel_vals,label="Kernel Values") 
+	ax.hist(actual_distances,bins=200,density=True,label="Distribution of Distances") 
+	ax.axvline(d_5,ls='--')
+	ax.axvline(d_95,ls='--')
+	ax.set_title("Inverse Multi-Quadratic (Dim="+str(dim)+")")
+	ax.legend() 
+	fig.savefig('../Graphs/g5_problem_InverseMultiQuadratic_dim='+str(dim)+'.png')
+
+
+
+for dim in dimensions:
+	sigma_data=1.0
+	actual_data = np.random.multivariate_normal(mean=np.zeros(dim), cov=sigma_data**2 * np.eye(dim), size=N) 
+	actual_distances = df1(actual_data) 
+	d_5, d_50, d_95 = np.percentile(actual_distances,5),np.percentile(actual_distances,50),np.percentile(actual_distances,95)
+
+	dist_vals = np.linspace( 0, np.max(actual_distances)+5 , 100)
+	kernel_vals = cauchy_kernel(dist_vals,s=d_50) 
+	kernel_vals = kernel_vals/np.max(kernel_vals)
+
+	fig,ax=plt.subplots()
+	ax.plot(dist_vals,kernel_vals,label="Kernel Values") 
+	ax.hist(actual_distances,bins=200,density=True,label="Distribution of Distances") 
+	ax.axvline(d_5,ls='--')
+	ax.axvline(d_95,ls='--')
+	ax.set_title("Cauchy (Dim="+str(dim)+")")
+	ax.legend() 
+	fig.savefig('../Graphs/g5_problem_Cauchy_dim='+str(dim)+'.png')
+
+
+for dim in dimensions:
+	sigma_data=1.0
+	actual_data = np.random.multivariate_normal(mean=np.zeros(dim), cov=sigma_data**2 * np.eye(dim), size=N) 
+	actual_distances = df1(actual_data) 
+	d_5, d_50, d_95 = np.percentile(actual_distances,5),np.percentile(actual_distances,50),np.percentile(actual_distances,95)
+
+	dist_vals = np.linspace( 0, np.max(actual_distances)+5 , 100)
+	kernel_vals = matern_kernel(dist_vals,l=d_50) 
+	kernel_vals = kernel_vals/np.max(kernel_vals)
+
+	fig,ax=plt.subplots()
+	ax.plot(dist_vals,kernel_vals,label="Kernel Values") 
+	ax.hist(actual_distances,bins=200,density=True,label="Distribution of Distances") 
+	ax.axvline(d_5,ls='--')
+	ax.axvline(d_95,ls='--')
+	ax.set_title("Matern Kernel (Dim="+str(dim)+")")
+	ax.legend() 
+	fig.savefig('../Graphs/g5_problem_Matern_dim='+str(dim)+'.png')
+
+for dim in dimensions:
+	sigma_data=1.0
+	actual_data = np.random.multivariate_normal(mean=np.zeros(dim), cov=sigma_data**2 * np.eye(dim), size=N) 
+	actual_distances = df1(actual_data) 
+	d_5, d_50, d_95 = np.percentile(actual_distances,5),np.percentile(actual_distances,50),np.percentile(actual_distances,95)
+
+	dist_vals = np.linspace( 0, np.max(actual_distances)+5 , 100)
+	kernel_vals = laplace_kernel(dist_vals,sigma=d_50/(-np.log(0.5))) 
+	kernel_vals = kernel_vals/np.max(kernel_vals)
+
+	fig,ax=plt.subplots()
+	ax.plot(dist_vals,kernel_vals,label="Kernel Values") 
+	ax.hist(actual_distances,bins=200,density=True,label="Distribution of Distances") 
+	ax.axvline(d_5,ls='--')
+	ax.axvline(d_95,ls='--')
+	ax.set_title("LaPlace Kernel (Dim="+str(dim)+")")
+	ax.legend() 
+	fig.savefig('../Graphs/g5_problem_LaPlace_dim='+str(dim)+'.png')
+
+
+# plt.show()
+
+
--- Presentation 1/Code/kernel_implementations.py
+++ Presentation 1/Code/kernel_implementations.py
@@ -17,7 +17,32 @@ def p_gaussian_kernel(distances,p=10.0,sigma=1.0):
 	kernel_vals = np.exp( - (distances**p)/(sigma**p) ) 
 	return kernel_vals 
 
+def polynomial_kernel(distances,a=1,b=0,p=2):
+	a = 1/len(distances)
+	kernel_vals = (a*distances+b)**p 
+	kernel_vals = -kernel_vals + np.max(kernel_vals)
+	return kernel_vals
+# Problem : Must decrease with distance 
 
+def rational_quadratic_kernel(distances,c=1):
+	kernel_vals = 1 - (distances**2)/((distances**2)+c)
+	return kernel_vals 
+
+def inverse_multiquadratic_kernel(distances,c=1):
+	kernel_vals = 1/ (( distances**2 + c**2 )**0.5)
+	return kernel_vals
+
+def cauchy_kernel(distances,s=1):
+	kernel_vals = 1 / (1 + (distances**2 / s**2))
+	return kernel_vals 
+
+def matern_kernel(distances,l=1.0):
+	kernel_vals = (1 + (5**0.5)*distances/l + 5*(distances**2)/(3*l**2))*np.exp( -(5**0.5)*distances/l ) 
+	return kernel_vals 
+
+def laplace_kernel(distances,sigma=1.0):
+	kernel_vals = np.exp( - (distances)/(sigma) ) 
+	return kernel_vals 
 
 
 
