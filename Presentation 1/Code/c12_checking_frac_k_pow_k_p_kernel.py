2cb1a5c28e646d94966474e2a2a9f30bca26535e
Harsh <harshraj11584@gmail.com>
2020-01-21 22:53:16 +0530

    added psd check for p-gaussian

--- Presentation 1/Code/c12_checking_frac_k_pow_k_p_kernel.py
+++ Presentation 1/Code/c12_checking_frac_k_pow_k_p_kernel.py
@@ -1,52 +1,52 @@
 import numpy as np 
-import itertools
 from scipy.linalg import eigh
 from multiprocessing import Pool 
 
 
-num_trials = 4
+num_parrallel_proc = 8
 N = int(1e3)
 k = 2.0
 
 count = 0 
 
+
 def p_gaussian_kernel(distances,p=10.0,sigma=1.0):
-	kernel_vals = np.exp( - (distances**p)/(sigma**p) ) 
+	kernel_vals = np.exp( - ((distances/sigma)**p) ) 
 	return kernel_vals 
 
 def do_trial(dim):
 	
 	data = np.random.uniform(low=-1000.0,high=1000.0,size=(N,dim))
-	# print("Trial Num= ",i+1,"Dimension=",dim)
+	# print(np.mean(data,axis=0).shape)
+	all_distances = np.linalg.norm(data-np.mean(data,axis=0),ord=k,axis=1)
 
-	dist_mat = list(itertools.product(np.arange(N),repeat=2))
-	# print(dist_mat)
-	elem_mat = data[dist_mat]
-	# print(elem_mat)
-	# print(elem_mat.shape)
+	d_5, d_50, d_95 = np.percentile(all_distances,5),np.percentile(all_distances,50),np.percentile(all_distances,95)
+	
+	# Uncomment one of these, alter between p-Gaussian and Gaussian Kernel
+	p = int(np.log(np.log(0.05)/np.log(0.95)) / np.log(d_95/d_5)) 	
+	# p = 1
 
-	diff_mat = elem_mat[:,0,:] - elem_mat[:,1,:]
-	# print(diff_mat.shape)
-	norm_vals = np.linalg.norm(diff_mat,axis=1,ord=k)
-	# print(norm_vals.shape)
-	d_5, d_50, d_95 = np.percentile(norm_vals,5),np.percentile(norm_vals,50),np.percentile(norm_vals,95)
-	p = np.log(np.log(0.05)/np.log(0.95)) / np.log(d_95/d_5) 
+
 	sigma = d_50/((-np.log(0.50))**(1/p))
-	kernel_vals = p_gaussian_kernel(norm_vals,p,sigma) 
-	K_mat = kernel_vals.reshape((N,N))
 
-	lowest_eig_val = eigh(K_mat,eigvals_only=True,eigvals=(0,0),overwrite_a=True)[0]
+	K_mat = np.zeros((N,N))
+	for i in range(N):
+		for j in range(i,N):
+			K_mat[i,j] = p_gaussian_kernel(np.linalg.norm(data[i]-data[j],ord=k),p,sigma)
+			K_mat[j,i] = K_mat[i,j]
+
+	lowest_eig_val = eigh(K_mat,eigvals_only=True,eigvals=(0,0))[0]
 	global count 
 	count = count+1
-	print("Trial Num= ",count,"Dimension=",dim,"Lowest EigVal = ",lowest_eig_val)
+	print("Tried Dim=",dim,"p=",p,"Lowest EigVal = ",lowest_eig_val)
 	return lowest_eig_val
 
-
-
+# N = 10
+# do_trial(100)
 
 while(True and count < 100):
-	proc = Pool(num_trials)
-	res = proc.map(do_trial,np.random.randint(low=5,high=1001,size=num_trials)	)
+	proc = Pool(num_parrallel_proc)
+	res = proc.map(do_trial,np.random.randint(low=5,high=1001,size=num_parrallel_proc)	)
 	proc.close() 
 	if(min(res)<0):
 		print("Found eigval<100, breaking")
